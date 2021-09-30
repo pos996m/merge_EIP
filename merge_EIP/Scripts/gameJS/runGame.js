fc42bb839117629b051a1a6e2272884db44be96f
pos996m <pos996m@gmail.com>
2021-10-01 04:53:31 +0800

    Update: Game

---
+++ merge_EIP/Scripts/gameJS/runGame.js
@@ -0,0 +1,331 @@
+﻿function RungameShow() {
+    Swal.fire({
+        showConfirmButton: false,
+        html: `<div id="myrungame"></div>`,
+        showCloseButton: true
+    })
+    Rungame();
+}
+
+function Rungame() {
+
+
+    const w = 812;
+    const h = 375;
+    let sorceInt = 0;
+
+    // 遊戲開始
+    const gameStart = {
+        key: 'gameStart',
+        preload: function () {
+
+            this.load.image('bg1', '/Content/images/mengo/bg/bg1_1.png');
+            this.load.image('bg2', '/Content/images/mengo/bg/bg2_1.png');
+            this.load.image('bg3', '/Content/images/mengo/bg/bg3_1.png');
+            this.load.image('bg4', '/Content/images/mengo/bg/bg4_1.png');
+            this.load.image('footer', '/Content/images/mengo/bg/footer_1.png');
+
+            this.load.image('logo', '/Content/images/mengo/ui/txt-title_1.png');
+            this.load.image('startBtn', '/Content/images/mengo/ui/btn-press-start_1.png');
+            this.load.image('playerEnd', '/Content/images/mengo/ui/player-end_1.png');
+
+        },
+        // 加入遊戲物件與相關設定
+        create: function () {
+            game.scale.pageAlignHorizontally = true;
+            game.scale.pageAlignVertically = true;
+
+            // 呈現場景
+            this.bg4 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg4');
+            this.bg3 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg3');
+            this.bg2 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg2');
+            this.bg1 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg1');
+            this.footer = this.add.tileSprite(w / 2, 405, w, 90, 'footer');
+
+            this.playerEnd = this.add.image(w / 2, 310, 'playerEnd');
+            this.playerEnd.setScale(0.5);
+            this.logo = this.add.image(w / 2, h / 2 - 100, 'logo');
+            this.logo.setScale(0.5);
+            this.startBtn = this.add.image(w / 2, h / 2, 'startBtn');
+            this.startBtn.setScale(0.7);
+
+            this.startBtn.setInteractive();
+
+            this.startBtn.on('pointerdown', () => {
+                this.scene.start('gamePlay');
+            });
+        },
+        // 遊戲狀態更新
+        update: function () {
+            this.bg3.tilePositionX += 3;
+            this.bg2.tilePositionX += 2;
+            this.bg1.tilePositionX += 4;
+            this.footer.tilePositionX += 4;
+        }
+    }
+
+    const getRandom = (max, min) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    const gamePlay = {
+        key: 'gamePlay',
+        // 載入資源
+        preload: function () {
+            // 圖片
+            // 背景
+            this.load.image('bg1', '/Content/images/mengo/bg/bg1_1.png');
+            this.load.image('bg2', '/Content/images/mengo/bg/bg2_1.png');
+            this.load.image('bg3', '/Content/images/mengo/bg/bg3_1.png');
+            this.load.image('bg4', '/Content/images/mengo/bg/bg4_1.png');
+            this.load.image('footer', '/Content/images/mengo/bg/footer_1.png');
+            // 載入音效
+            this.load.audio('jump12', '/Content/images/gameAudio/jump12.mp3');
+            this.load.audio('touch', '/Content/images/gameAudio/touch.mp3');
+            // jumpBtn
+            // this.load.image('btnJump', '../images/ui/btn-jump.png');
+            // monster
+            this.load.image('rock1', '/Content/images/mengo/showman.png');
+            this.load.image('rock2', '/Content/images/mengo/showman.png');
+            this.load.image('rock3', '/Content/images/mengo/showman.png');
+            // 遊戲結束後的標題
+            this.load.image('gameover', '/Content/images/mengo/ui/txt-game-over_1.png');
+            this.load.image('tryAgainBtn', '/Content/images/mengo/ui/btn-try-again_1.png');
+            this.load.image('playAgainBtn', '/Content/images/mengo/ui/btn-play-again_1.png');
+            // 腳色動畫
+            this.load.spritesheet('user', '/Content/images/mengo/f_player.png', { frameWidth: 144, frameHeight: 120 });
+
+            // 時間
+            this.timeInt = 0;
+            // 分數 
+
+            // 遊戲是否停止
+            this.gameStop = false;
+            // 是否可以跳躍
+            this.iskeyJump = true;
+            // 存放所有怪物實體
+            this.monsterArr = [];
+            // 存放所有怪物實體2    
+            this.monsterArr2 = [];
+            // 怪物索引 1
+            this.masIdx = 0;
+            // 怪物索引2
+            this.masIdx2 = 1;
+            // 速度
+            this.bgSpeed = 1.3;
+        },
+        // 加入遊戲物件與相關設定
+        create: function () {
+            // 將圖片背景放置
+            this.bg4 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg4');
+            this.bg3 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg3');
+            this.bg2 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg2');
+            this.bg1 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg1');
+            this.footer = this.add.tileSprite(w / 2, 405, w, 90, 'footer');
+
+            // 設定腳色sprite
+            this.player = this.physics.add.sprite(105, 105, 'user');
+            // 縮小腳色尺寸
+            this.player.setScale(0.7);
+            // 腳色邊界限制
+            this.player.setCollideWorldBounds(true);
+            // 設定腳色碰撞範圍
+            this.player.setSize(105, 115);
+            // 設定人物與地板物理接觸
+            this.physics.add.collider(this.player, this.footer);
+
+            // 將地板加入物理效果
+            this.physics.add.existing(this.footer);
+            // 設定地板不因加入物理重力影響
+            this.footer.body.immovable = true;
+            // 設定物件位置旋轉是否受影響
+            this.footer.body.moves = false;
+
+            // 加入怪物物理效果
+            const addPhysics = GameObject => {
+                this.physics.add.existing(GameObject);
+                GameObject.body.immovable = true;
+                GameObject.body.moves = false;
+            }
+
+            // 走路 一般狀態
+            this.anims.create({
+                key: 'run',
+                frames: this.anims.generateFrameNumbers('user', { start: 0, end: 1 }),
+                frameRate: 5,
+                repeat: -1
+            });
+            // 跳
+            this.anims.create({
+                key: 'jump',
+                frames: this.anims.generateFrameNumbers('user', { start: 2, end: 2 }),
+                frameRate: 5,
+                repeat: -1
+            });
+            // 飛起
+            this.anims.create({
+                key: 'landing',
+                frames: this.anims.generateFrameNumbers('user', { start: 3, end: 3 }),
+                frameRate: 5,
+                repeat: -1
+            });
+            // deal
+            this.anims.create({
+                key: 'deal',
+                frames: this.anims.generateFrameNumbers('user', { start: 4, end: 4 }),
+                frameRate: 5,
+                repeat: -1
+            });
+
+            // 怪物的座標資訊
+            const masPos = [
+                { name: 'rock1', x: w + 200, y: 320, w: 160, h: 83 },
+                { name: 'rock2', x: w + 200, y: 320, w: 160, h: 83 },
+                { name: 'rock3', x: w + 200, y: 320, w: 160, h: 83 },
+            ]
+
+            // 撞到一次寫一次if判斷
+            var myAPItrue = true;
+
+            //碰撞到後停止遊戲
+            const hittest = (player, rock) => {
+                let gameURL = `?sorce=${sorceInt}`;
+                if (myAPItrue) {
+                    console.log("撞到後會顯示嗎?")
+                    // 呼叫API
+                    myAPItrue = false;
+                }
+                this.sound.play('touch');
+                this.gameStop = true;
+                this.player.setSize(110, 100, 0);
+                // deal圖
+                this.player.anims.play('deal', true);
+                clearInterval(timer);
+                this.gameover = this.add.image(w / 2, h / 2 - 40, 'gameover');
+                this.gameover.setScale(0.8);
+                this.tryAgainBtn = this.add.image(w / 2, h / 2 + 30, 'tryAgainBtn');
+                this.tryAgainBtn.setScale(0.6);
+                this.tryAgainBtn.setInteractive();
+                this.tryAgainBtn.on('pointerdown', () => {
+                    // 這邊是按下按鈕會回傳
+                    myAPItrue = true;
+                    // 刪除原本的
+                    $("#myrungame>canvas").remove();
+                    // 新建一個新的
+                    sorceInt = 0;
+                    game = new Phaser.Game(config);
+                    //window.location.href += gameURL;
+                });
+
+                this.sound.stopAll();
+            }
+
+            // 產生怪物
+            for (let i = 0; i < 10; i++) {
+                let BoolIdx = getRandom(2, 0);
+                let BoolIdx2 = getRandom(2, 0);
+                this['rock' + i] = this.add.tileSprite(masPos[BoolIdx].x, masPos[BoolIdx].y, masPos[BoolIdx].w, masPos[BoolIdx].h, masPos[BoolIdx].name);
+                this['rockB' + i] = this.add.tileSprite(masPos[BoolIdx2].x, masPos[BoolIdx2].y, masPos[BoolIdx2].w, masPos[BoolIdx2].h, masPos[BoolIdx2].name);
+                this.monsterArr.push(this['rock' + i]);
+                this.monsterArr2.push(this['rockB' + i]);
+                this['rock' + i].setScale(0.4);
+                this['rockB' + i].setScale(0.4);
+                this['rock' + i].setSize(170, 200, 0);
+                this['rockB' + i].setSize(170, 200, 0);
+                addPhysics(this['rock' + i]);
+                addPhysics(this['rockB' + i]);
+                this.physics.add.collider(this.player, this['rock' + i], hittest);
+                this.physics.add.collider(this.player, this['rockB' + i], hittest);
+            }
+
+            // 時間倒數
+            this.TimeText = this.add.text(50, 0, `Time:${this.timeInt}`, { color: '#fff', fontSize: '30px' })
+            // 計算分數
+            this.sorceText = this.add.text(340, 0, `Sorce:${sorceInt}`, { color: '#fff', fontSize: '30px' })
+            // 計時
+            let timer = setInterval(() => {
+                this.timeInt++;
+                sorceInt += 100;
+                //console.log("HI" + sorceInt)
+                this.TimeText.setText(`Time:${this.timeInt}`);
+                this.sorceText.setText(`Sorce:${sorceInt}`);
+                if ((this.timeInt % 10) == 0) {
+                    // 每10秒加速度
+                    this.bgSpeed += 0.2;
+                }
+            }, 1000);
+
+            // this.player.anims.play('run', true);
+        },
+        // 遊戲狀態更新
+        update: function () {
+            // 當Stop為true 則停止update
+            if (this.gameStop) return;
+
+            // 場景移動
+            this.bg3.tilePositionX += 3 * this.bgSpeed;
+            this.bg2.tilePositionX += 2 * this.bgSpeed;
+            this.bg1.tilePositionX += 4 * this.bgSpeed;
+            this.footer.tilePositionX += 4 * this.bgSpeed;
+            // 怪物移動
+            this.monsterArr[this.masIdx].x -= 6 * this.bgSpeed;
+            // 當時間來到50秒就增加一個怪物
+            if (this.timeInt > 50) {
+                this.monsterArr2[this.masIdx2].x -= 6 * this.bgSpeed;
+            }
+
+            // 檢測怪物是否超出邊界然後返回
+            for (let i = 0; i < this.monsterArr.length; i++) {
+                if (this.monsterArr[i].x <= -100) {
+                    this.monsterArr[i].x = w + 200;
+                    this.masIdx = getRandom(this.monsterArr.length - 1, 0);
+                }
+                if (this.monsterArr2[i].x <= -100) {
+                    this.monsterArr2[i].x = w + getRandom(400, 200);
+                    this.masIdx2 = getRandom(this.monsterArr2.length - 1, 0);
+                }
+            }
+
+            // 滑鼠點擊
+            let pointer = this.input.activePointer;
+            // 鍵盤輸入
+            let keyboard = this.input.keyboard.createCursorKeys();
+            // 當接觸地面時才可以起跳
+            if (this.player.body.blocked.down && (keyboard.up.isDown || pointer.isDown)) {
+                this.sound.play('jump12');
+                this.player.anims.play('jump', true);
+                // 跳躍高度
+                this.player.setVelocityY(-600);
+                // console.log('up');
+            } else {
+                // 回到地面時 回復走路狀態
+                if (this.player.body.blocked.down) {
+                    this.player.anims.play('run', true);
+                }
+            }
+        }
+    }
+
+    // 設定canvas
+    const config = {
+        type: Phaser.AUTO,
+        width: w,
+        height: h,
+        parent: 'myrungame',
+        // 加入物理引擎
+        physics: {
+            default: 'arcade',
+            arcade: {
+                // 重力值
+                gravity: {
+                    y: 1100
+                },
+                // 查看物件碰撞範圍
+                // debug: true
+            },
+        },
+        // 場景 兩個 start play
+        scene: [gameStart, gamePlay]
+    }
+    let game = new Phaser.Game(config);
+}
---
+++ merge_EIP/Scripts/gameJS/snakegmae.js
@@ -0,0 +1,366 @@
+﻿function snykebtn() {
+    Swal.fire({
+        showConfirmButton: false,
+        html: `<div id="phaser"></div>`,
+        showCloseButton: true
+    })
+    snykegame();
+}
+
+function snykegame() {
+
+    // 主體
+    var cat;
+    // 食物
+    var food;
+    // 鍵盤
+    var keyboard;
+    // 點
+    let pointer;
+    // 計數器
+    let timer;
+    // 時間
+    let timeInt = 0;
+    // 分數
+    let sorceInt = 0;
+    // 方向判斷
+    let x = 1;
+    let y = 0;
+    // 解決觸碰連點
+    var cnt = 0;
+    // 遊戲尺寸
+    const w = 640;
+    const h = 500;
+    // 主體與食物的大小
+    const block = 20;
+    // 寬與高的格數限制
+    const weightBlock = 32; //32
+    const hightBlock = 22; //22
+    // 進入控制器
+    let sorceTrue = true;
+
+
+    // 遊戲開始畫面
+    const gameStar = {
+        key: 'gameStar',
+        preload: function () {
+            this.load.image('bg', '/Content/images/cateat/bg_2.png')
+            this.load.image('start_title', '/Content/images/cateat/start_2.png');
+            this.load.image('start_click', '/Content/images/cateat/startgame_2.png');
+        },
+        create: function () {
+            this.bg = this.add.image(340, 240, 'bg')
+            this.start_title = this.add.image(320, 240, 'start_title')
+            this.start_click = this.add.image(320, 240, 'start_click')
+            this.start_click.setInteractive();
+            this.start_click.on('pointerdown', () => this.scene.start('gamePlay'));
+        },
+        update: function () {
+
+        }
+    }
+    // 遊戲畫面
+    const gamePlay = {
+        key: 'gamePlay',
+        preload: function () {
+            this.load.image('cat', '/Content/images/cateat/cat_2.png');
+            this.load.image('love', '/Content/images/cateat/love_2.png');
+            this.load.image('bg', '/Content/images/cateat/bg_2.png')
+            this.load.image('over_title', '/Content/images/cateat/over_2.png');
+            this.load.image('try_again', '/Content/images/cateat/tryagain.png');
+            this.load.image('back_menu', '/Content/images/cateat/backmenu.png');
+            this.load.audio('eat', '/Content/images/gameAudio/eat.mp3');
+        },
+        create: function () {
+            this.bg = this.add.image(340, 240, 'bg')
+            var Love = new Phaser.Class({
+
+                Extends: Phaser.GameObjects.Image,
+
+                initialize:
+
+                    function Love(scene, x, y) {
+                        Phaser.GameObjects.Image.call(this, scene)
+
+                        this.setTexture('love');
+                        this.setPosition(x * block, y * block);
+                        this.setOrigin(0);
+
+                        this.total = 3;
+
+                        scene.children.add(this);
+                    },
+
+                eat: function () {
+                    sorceInt += 100;
+                    this.total++;
+
+                }
+
+            });
+
+            var Cat = new Phaser.Class({
+
+                initialize:
+
+                    function Cat(scene, x, y) {
+                        this.headPosition = new Phaser.Geom.Point(x, y);
+                        this.body = scene.add.group();
+                        this.head = this.body.create(x * block, y * block, 'cat');
+                        this.head.setOrigin(0);
+                        this.alive = true;
+                        this.speed = 100;
+                        this.moveTime = 0;
+                        this.tail = new Phaser.Geom.Point(x, y);
+                    },
+
+                update: function (time) {
+                    if (time >= this.moveTime) {
+                        return this.move(time);
+                    }
+                },
+                move: function (time) {
+                    this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x + x, 0, weightBlock);
+                    this.headPosition.y = Phaser.Math.Wrap(this.headPosition.y + y, 0, hightBlock);
+
+                    Phaser.Actions.ShiftPosition(this.body.getChildren(), this.headPosition.x * block, this.headPosition.y * block, 1, this.tail);
+
+
+
+                    var hitBody = Phaser.Actions.GetFirst(this.body.getChildren(), { x: this.head.x, y: this.head.y }, 1);
+
+                    if (hitBody) {
+
+                        console.log('dead');
+
+                        this.alive = false;
+
+                        return false;
+                    }
+                    else {
+
+                        this.moveTime = time + this.speed;
+
+                        return true;
+                    }
+                },
+
+                grow: function () {
+                    var newPart = this.body.create(this.tail.x, this.tail.y, 'cat');
+
+                    newPart.setOrigin(0);
+                },
+
+                collideWithFood: function (food) {
+                    if (this.head.x === food.x && this.head.y === food.y) {
+                        this.grow();
+
+                        food.eat();
+
+
+                        if (this.speed > 20 && food.total % 5 === 0) {
+                            this.speed -= 5;
+                        }
+
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                },
+
+                updateGrid: function (grid) {
+                    this.body.children.each(function (segment) {
+
+                        var bx = segment.x / block;
+                        var by = segment.y / block;
+
+                        grid[by][bx] = false;
+
+                    });
+                    return grid;
+                }
+
+            });
+
+            food = new Love(this, 3, 4);
+
+            cat = new Cat(this, 8, 8);
+
+            this.cursors = this.input.keyboard.addKeys({
+                leftKey: Phaser.Input.Keyboard.KeyCodes.LEFT,
+                rightKey: Phaser.Input.Keyboard.KeyCodes.RIGHT
+            });
+            pointer = this.input.activePointer;
+            // 計算分數
+            this.sorceText = this.add.text(400, 460, `Sorce:${sorceInt}`, { color: '#fff', fontSize: '30px' });
+            this.TimeText = this.add.text(60, 460, `Time:${timeInt}`, { color: '#fff', fontSize: '30px' })
+            // 計時
+            timer = setInterval(() => {
+                timeInt++;
+                this.TimeText.setText(`Time:${timeInt}`);
+            }, 1000);
+
+        },
+        update: function (time) {
+            // 分數更新 顯示
+            this.sorceText.setText(`Sorce:${sorceInt}`);
+            // 接分數，最後用來傳入資料庫
+            let sorce = sorceInt;
+            let tryURL = `?sorce=${sorce}&page=${"again"}`;
+            let backURL = `?sorce=${sorce}&page=${"back"}`;
+            if (!cat.alive) {
+                if (sorceTrue) {
+                    // 放呼叫API
+                    console.log("HI 我撞到了");
+                    sorceTrue = false;
+                }
+                this.over_title = this.add.image(330, 240, 'over_title');
+                this.try_again = this.add.image(330, 270, 'try_again');
+                //this.back_menu = this.add.image(500, 300, 'back_menu');
+                this.try_again.setInteractive();
+                this.try_again.on('pointerdown',
+                    () => {
+                        //this.scene.start('gamePlay');
+                        console.log(tryURL);
+                        //window.location.href += tryURL;
+                        sorceInt = 0;
+                        timeInt = 0;
+                        $("#phaser>canvas").remove();
+                        game = new Phaser.Game(config);
+                    });
+                //this.back_menu.setInteractive();
+                //this.back_menu.on('pointerdown',
+                //    () => {
+                //        //this.scene.start('gamePlay');
+                //        window.location.href += backURL;
+                //        sorceInt = 0;
+                //        timeInt = 0;
+                //    });
+                clearInterval(timer);
+                return;
+            }
+            function key_touch_left() {
+                if (x === 1 && y === 0) {
+                    // cat.faceUp();
+                    x = 0;
+                    y = -1;
+                    console.log(x, y, "up");
+                } else if (x === 0 && y === -1) {
+                    // cat.faceLeft();
+                    x = -1;
+                    y = 0;
+                    console.log(x, y, "left");
+                } else if (x === -1 && y === 0) {
+                    // cat.faceDown();
+                    x = 0;
+                    y = 1;
+                    console.log(x, y, "down");
+                } else if (x === 0 && y === 1) {
+                    // cat.faceRight();
+                    x = 1;
+                    y = 0;
+                    console.log(x, y, "Right");
+                }
+            }
+            function key_touch_right() {
+                if (x === 1 && y === 0) {
+                    // cat.faceDown();
+                    x = 0;
+                    y = 1;
+                    console.log(x, y, "up");
+                } else if (x === 0 && y === 1) {
+                    // cat.faceLeft();
+                    x = -1;
+                    y = 0;
+                    console.log(x, y, "left");
+                } else if (x === -1 && y === 0) {
+                    // cat.faceUp();
+                    x = 0;
+                    y = -1;
+                    console.log(x, y, "down");
+                } else if (x === 0 && y === -1) {
+                    // cat.faceRight();
+                    x = 1;
+                    y = 0;
+                    console.log(x, y, "Right");
+                }
+            }
+            //Touch
+            if (pointer.leftButtonReleased()) {
+                cnt = 0;
+            }
+            if (pointer.isDown && (pointer.x < 400 && pointer.x > 0)) {
+                cnt++;
+                if (cnt < 2) {
+                    key_touch_left()
+                }
+            }
+            else if (pointer.isDown && (pointer.x < 800 && pointer.x > 400)) {
+                cnt++;
+                if (cnt < 2) {
+                    key_touch_right()
+                }
+            }
+
+            // 鍵盤
+            const { leftKey, rightKey } = this.cursors;
+            if (Phaser.Input.Keyboard.JustDown(leftKey)) {
+                key_touch_left();
+            }
+            else if (Phaser.Input.Keyboard.JustDown(rightKey)) {
+                key_touch_right();
+            }
+
+            if (cat.update(time)) {
+                if (cat.collideWithFood(food)) {
+                    this.sound.play('eat');
+                    repositionFood();
+                }
+            }
+
+            function repositionFood() {
+                var testGrid = [];
+                for (var y = 0; y < hightBlock; y++) {
+                    testGrid[y] = [];
+
+                    for (var x = 0; x < weightBlock; x++) {
+                        testGrid[y][x] = true;
+                    }
+                }
+                cat.updateGrid(testGrid);
+                var validLocations = [];
+                for (var y = 0; y < hightBlock; y++) {
+                    for (var x = 0; x < weightBlock; x++) {
+                        if (testGrid[y][x] === true) {
+                            validLocations.push({ x: x, y: y });
+                        }
+                    }
+                }
+                if (validLocations.length > 0) {
+                    var pos = Phaser.Math.RND.pick(validLocations);
+                    food.setPosition(pos.x * block, pos.y * block);
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    }
+
+
+    var config = {
+        type: Phaser.AUTO,
+        width: w,
+        height: h,
+        backgroundColor: '#5B5B5B',
+        parent: 'phaser',
+        input: {
+            activePointers: 1
+        },
+        scene: [gameStar, gamePlay]
+    };
+
+    var game = new Phaser.Game(config);
+}
